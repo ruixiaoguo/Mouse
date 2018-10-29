ce1148b09b976e24c97f21b1a768b949169a9499
ruixiaoguo <grx0917@sina.com>
2018-10-29 17:44:38 +0800

    Initial commit

---
+++ Mouse/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Mouse
+//
+//  Created by grx on 2018/10/29.
+//  Copyright © 2018年 grx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.red
+
+    }
+
+
+}
+
